2e4bb45ac6b56bfaf1c7b7eeefdab25f852c50d8
Abdullah Baig <abdullahbaig456@gmail.com>
2024-04-22 21:43:53 +0200

    Use http-status-codes

--- src/interfaces/http/middleware/ErrorHandler.middleware.ts
+++ src/interfaces/http/middleware/ErrorHandler.middleware.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 export const errorHandler = (
   err: Error,
@@ -7,5 +8,5 @@ export const errorHandler = (
   _next: express.NextFunction,
 ) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Something broke!');
 };
